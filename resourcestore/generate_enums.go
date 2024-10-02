60029f5b46671516a41ee0491f10c711650de7c2
Blaine Swaney <blaine@cloud-team.com>
2024-10-02 15:51:56 -0600

    feat: Added functionality to generate enums for typescript (#59)

---
+++ resourcestore/generate_enums.go
@@ -0,0 +1,97 @@
+package resourcestore
+
+import (
+	"maps"
+	"os"
+	"slices"
+	"text/template"
+
+	"github.com/cccteam/ccc/accesstypes"
+	"github.com/go-playground/errors/v5"
+)
+
+const enumTemplate = `
+{{- range .}}
+export enum {{.Name}} {
+{{- range .Values}}
+  {{.}} = '{{.}}',
+{{- end}}
+}
+{{end}}
+`
+
+func (s *Store) TypeScriptPermissions(dst string) error {
+	perms := make(map[accesstypes.Permission]struct{})
+	resources := make(map[accesstypes.Resource]struct{})
+	for _, store := range s.resourceStore {
+		for resource, fMap := range store {
+			for _, perm := range fMap {
+				perms[perm] = struct{}{}
+				resources[resource] = struct{}{}
+			}
+		}
+	}
+
+	fields := make(map[accesstypes.Resource]map[accesstypes.Field]struct{})
+	for _, store := range s.fieldStore {
+		for r, resourceFields := range store {
+			for field, permissions := range resourceFields {
+				if fields[r] == nil {
+					fields[r] = make(map[accesstypes.Field]struct{})
+				}
+				fields[r][field] = struct{}{}
+				for _, perm := range permissions {
+					perms[perm] = struct{}{}
+				}
+			}
+		}
+	}
+
+	enums := []Enum{{
+		Name:   "Permissions",
+		Values: slices.Collect(maps.Keys(perms)),
+	}, {
+		Name:   "Resources",
+		Values: slices.Collect(maps.Keys(resources)),
+	}}
+
+	for resource, fields := range fields {
+		enums = append(enums, Enum{
+			Name:   string(resource),
+			Values: slices.Collect(maps.Keys(fields)),
+		})
+	}
+
+	if err := writeFile(dst, enums); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeFile(dst string, enums []Enum) error {
+	f, err := os.Create(dst)
+	if err != nil {
+		return errors.Wrap(err, "os.Create()")
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString("// This file is auto-generated. Do not edit manually."); err != nil {
+		return errors.Wrap(err, "f.WriteString()")
+	}
+
+	t := template.Must(template.New("typescript enums").Parse(enumTemplate))
+	if err := t.Execute(f, enums); err != nil {
+		return errors.Wrap(err, "t.Execute()")
+	}
+
+	if err := f.Close(); err != nil {
+		return errors.Wrap(err, "f.Close()")
+	}
+	return nil
+}
+
+type Enum struct {
+	Name   string
+	Values any
+}
